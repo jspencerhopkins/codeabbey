8bcb590908cb7f073d9d96bdb5c11c99012b2242
J. Spencer Hopkins <jspencerhopkins@gmail.com>
2015-05-11 12:12:56 -0400

    Problem 1 solved

---
+++ lib/problem-0001.rb
@@ -0,0 +1,10 @@
+# sum 8732104 8533926
+
+
+module Problem1
+  def self.answer(num1, num2)
+
+    num1 + num2
+
+  end
+end
---
+++ spec/problem-0001_spec.rb
@@ -0,0 +1,11 @@
+# sum 8732104 8533926
+
+require './lib/problem-0001'
+
+describe 'Sum of' do
+
+  it '8732104 + 8533926' do
+    expect(sum).to eq 17266030
+  end
+
+end
