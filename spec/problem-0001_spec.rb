d7de70da981de3b9479bf8e81c8b6bec574added
J. Spencer Hopkins <jspencerhopkins@gmail.com>
2015-05-11 12:13:54 -0400

    Forgot to save problem1 spec

--- spec/problem-0001_spec.rb
+++ spec/problem-0001_spec.rb
@@ -1,11 +1,11 @@
-# sum 8732104 8533926
+# sum 3528805 9205741
 
 require './lib/problem-0001'
 
 describe 'Sum of' do
 
   it '8732104 + 8533926' do
-    expect(sum).to eq 17266030
+    expect(Problem1.answer(3528805,9205741)).to eq 12734546
   end
 
 end
